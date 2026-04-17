65ad3c45b5c26668444b3a2eeb2715392542bdf5
CrocodielRussian <mk-spbkiselev@yandex.ru>
2026-04-17 08:54:22 +0300

    ref: move interface tests to self class

--- src/test/java/web/tests/MainInterfaceTests.java
+++ src/test/java/web/tests/MainInterfaceTests.java
@@ -1,4 +1,19 @@
 package web.tests;
 
-public class MainInterfaceTests {
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import web.base.BaseTest;
+
+public class MainInterfaceTests extends BaseTest {
+
+    @Test
+    @DisplayName("Функциональный тест загрузки главного интерфейса")
+    void testLoadingMainInterface() {
+        By dashboardLocator = By.xpath("//div[contains(@class, 'main-layout')]");
+        WebElement dashboard = loginWait.until(
+                ExpectedConditions.visibilityOfElementLocated(dashboardLocator)
+        );
+        System.out.println("Тест успешно пройден!");
+    }
 }
